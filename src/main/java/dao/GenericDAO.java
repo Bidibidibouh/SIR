f3e178b20f1c85adefbbe779772c2d42710e34eb
VincentPelletier <vincent.pelletier.72440@gmail.com>
2018-01-18 16:56:53 +0100

    test2

--- src/main/java/dao/GenericDAO.java
+++ src/main/java/dao/GenericDAO.java
@@ -1,10 +1,10 @@
-package dao;
+package src.main.java.dao;
 
 import java.io.Serializable;
 
 public interface GenericDAO<T, PK extends Serializable> {
     T create(T t);
-    T read(PK id);        //bitye
+    T read(PK id);      
     T update(T t);
     void delete(T t);
 }
