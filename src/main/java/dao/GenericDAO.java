8f8d171200bec157f084c40ebbb8125dbbd4307c
Bidibidibouh <aleexxhrt@hotmail.fr>
2018-01-18 16:51:56 +0100

    test

--- src/main/java/dao/GenericDAO.java
+++ src/main/java/dao/GenericDAO.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 public interface GenericDAO<T, PK extends Serializable> {
     T create(T t);
-    T read(PK id);
+    T read(PK id);        //bitye
     T update(T t);
     void delete(T t);
 }
